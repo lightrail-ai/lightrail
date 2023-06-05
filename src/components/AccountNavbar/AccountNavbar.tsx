6183c17257a4f56523293d60238eec4615fb7544
Vishnu Menon <me@vishnumenon.com>
2023-06-04 17:01:02 -0700

    Added logo to title

--- src/components/AccountNavbar/AccountNavbar.tsx
+++ src/components/AccountNavbar/AccountNavbar.tsx
@@ -6,7 +6,7 @@ export interface AccountNavbarProps {}
 function AccountNavbar({}: AccountNavbarProps) {
   return (
     <div className="pt-12 pb-8 mb-8 mx-20 px-4 flex flex-row border-b">
-      <span className="text-3xl font-semibold">Projects</span>
+      <span className="text-3xl font-semibold">🪄 Projects</span>
     </div>
   );
 }
