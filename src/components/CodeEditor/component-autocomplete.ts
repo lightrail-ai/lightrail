a3f1d8d39cf55688cb156a41c0e5c00b58cb32d8
Vishnu Menon <me@vishnumenon.com>
2023-06-15 17:11:08 -0700

    Added Tailwind Autocomplete

--- src/components/CodeEditor/component-autocomplete.ts
+++ src/components/CodeEditor/component-autocomplete.ts
@@ -6,192 +6,93 @@ import {
   Completion,
 } from "@codemirror/autocomplete";
 import { ComponentCreationCallback } from "../ProjectEditor/editor-types";
+import { syntaxTree } from "@codemirror/language";
+import { HTML_TAGS, TAILWIND_CLASSES } from "./autocomplete-data";
+import { SyntaxNode } from "@lezer/common";
 
-let htmlTags = [
-  "html",
-  "base",
-  "head",
-  "link",
-  "meta",
-  "script",
-  "style",
-  "title",
-  "body",
-  "address",
-  "article",
-  "aside",
-  "footer",
-  "header",
-  "h1",
-  "h2",
-  "h3",
-  "h4",
-  "h5",
-  "h6",
-  "hgroup",
-  "main",
-  "nav",
-  "section",
-  "blockquote",
-  "cite",
-  "dd",
-  "dt",
-  "dl",
-  "div",
-  "figcaption",
-  "figure",
-  "hr",
-  "li",
-  "ol",
-  "ul",
-  "menu",
-  "p",
-  "pre",
-  "a",
-  "abbr",
-  "b",
-  "bdi",
-  "bdo",
-  "br",
-  "code",
-  "data",
-  "dfn",
-  "em",
-  "i",
-  "kbd",
-  "mark",
-  "q",
-  "rp",
-  "ruby",
-  "rt",
-  "s",
-  "samp",
-  "small",
-  "span",
-  "strong",
-  "sub",
-  "sup",
-  "time",
-  "u",
-  "var",
-  "wbr",
-  "area",
-  "audio",
-  "img",
-  "map",
-  "track",
-  "video",
-  "embed",
-  "iframe",
-  "object",
-  "picture",
-  "source",
-  "portal",
-  "svg",
-  "canvas",
-  "noscript",
-  "del",
-  "ins",
-  "caption",
-  "col",
-  "colgroup",
-  "table",
-  "tbody",
-  "tr",
-  "td",
-  "tfoot",
-  "th",
-  "thead",
-  "button",
-  "datalist",
-  "option",
-  "fieldset",
-  "label",
-  "form",
-  "input",
-  "legend",
-  "meter",
-  "optgroup",
-  "select",
-  "output",
-  "progress",
-  "textarea",
-  "details",
-  "summary",
-  "dialog",
-  "slot",
-  "template",
-  "acronym",
-  "applet",
-  "bgsound",
-  "big",
-  "blink",
-  "center",
-  "dir",
-  "font",
-  "frame",
-  "frameset",
-  "image",
-  "keygen",
-  "marquee",
-  "menuitem",
-  "nobr",
-  "noembed",
-  "noframes",
-  "param",
-  "plaintext",
-  "rb",
-  "rtc",
-  "spacer",
-  "strike",
-  "tt",
-  "xmp",
-];
+const TAILWIND_COMPLETIONS = TAILWIND_CLASSES.map((className) => ({
+  label: className,
+  type: "class",
+}));
 
 function projectCompletions(
   project: ProjectWithFiles | undefined,
   onCreateComponent: (name: string, callback: ComponentCreationCallback) => void
 ): CompletionSource {
   return (context: CompletionContext) => {
+    // Component Name autocomplete:
+
     let word = context.matchBefore(/<\w*/);
-    if (!word || (word.from == word.to && !context.explicit)) return null;
-    const components = project?.files?.map((file) => file.path) ?? [];
-    let completions: Completion[] = [
-      {
-        label: word.text.substring(1),
-        detail: "(Create New Component...)",
-        apply(view, completion, from, to) {
-          onCreateComponent(word?.text.substring(1) ?? "", (name, props) => {
-            view.dispatch({
-              changes: {
-                from,
-                to,
-                insert: `${name} ${props
-                  .map((p) => `${p}={undefined}`)
-                  .join(" ")} />`,
-              },
+    if (word && (word.from != word.to || context.explicit)) {
+      const components = project?.files?.map((file) => file.path) ?? [];
+      let completions: Completion[] = [
+        {
+          label: word.text.substring(1),
+          detail: "(Create New Component...)",
+          apply(view, completion, from, to) {
+            onCreateComponent(word?.text.substring(1) ?? "", (name, props) => {
+              view.dispatch({
+                changes: {
+                  from,
+                  to,
+                  insert: `${name} ${props
+                    .map((p) => `${p}={undefined}`)
+                    .join(" ")} />`,
+                },
+              });
             });
-          });
+          },
         },
-      },
-    ];
+      ];
 
-    completions = completions.concat(
-      components.map((name) => ({
-        label: name,
-        boost: 1,
-      }))
-    );
-    completions = completions.concat(
-      htmlTags.map((tag) => ({
-        label: tag,
-      }))
+      completions = completions.concat(
+        components.map((name) => ({
+          label: name,
+          boost: 1,
+        }))
+      );
+      completions = completions.concat(
+        HTML_TAGS.map((tag) => ({
+          label: tag,
+        }))
+      );
+
+      return {
+        from: word.from + 1,
+        options: completions,
+      };
+    }
+
+    // Tailwind autocomplete:
+
+    let curr: SyntaxNode | null = syntaxTree(context.state).resolveInner(
+      context.pos
     );
 
-    return {
-      from: word.from + 1,
-      options: completions,
-    };
+    while (curr) {
+      if (curr.name === "JSXAttribute") {
+        break;
+      }
+      curr = curr.parent;
+    }
+
+    if (curr) {
+      word = context.matchBefore(/[a-z0-9-]*/);
+      const nodeText = context.state.doc.sliceString(curr.from, curr.to);
+      if (
+        nodeText.startsWith("className") &&
+        context.pos - curr.from > 10 &&
+        word &&
+        (word.from != word.to || context.explicit)
+      ) {
+        return {
+          from: word.from,
+          options: TAILWIND_COMPLETIONS,
+        };
+      }
+    }
+
+    return null;
   };
 }
 
