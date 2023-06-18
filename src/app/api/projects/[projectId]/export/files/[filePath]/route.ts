e97a14065f816fc0c931fa8df9aa18162c058a2f
Vishnu Menon <me@vishnumenon.com>
2023-06-17 19:28:39 -0700

    Added state to exported components

--- src/app/api/projects/[projectId]/export/files/[filePath]/route.ts
+++ src/app/api/projects/[projectId]/export/files/[filePath]/route.ts
@@ -1,4 +1,4 @@
-import { Client, File } from "@/util/storage";
+import { Client, File, FileStateItem } from "@/util/storage";
 import { cookies } from "next/headers";
 
 function createExportComponent(file: File) {
@@ -8,12 +8,25 @@ function createExportComponent(file: File) {
     )
   );
 
+  const stateDeclarations = file.state
+    ? (file.state as unknown as FileStateItem[])
+        .map(
+          (s) =>
+            `const [${s.name}, ${
+              "set" + s.name.charAt(0).toUpperCase() + s.name.slice(1)
+            }] = React.useState(${s.initial});`
+        )
+        .join("\n")
+    : "";
+
   return `import React from "react";
+
 ${componentImports
   .map((comp) => `import ${comp} from "@/components/${comp}";`)
   .join("\n")}
 
 export default function ${file.path}(props) {
+    ${stateDeclarations}
     return (<>${file.contents!}</>);
 } `;
 }
